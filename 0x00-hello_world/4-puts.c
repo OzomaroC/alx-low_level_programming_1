b4cc0113391af7169afc082163c533735fc045ea
OzomaroC <ozomarocyril@gmail.com>
2022-10-13 21:45:42 -0700

    c language

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-	puts("Programming is like building is like building a multilignual puzzle\n");
+	puts("\"Programming is like building is like building a multilignual
+puzzle"");
 	return (0);
 }
