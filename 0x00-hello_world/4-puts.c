f450c044058ab7d66f13f2a0222f1e891b65c671
OzomaroC <ozomarocyril@gmail.com>
2022-10-13 20:18:20 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building is like building a multilignual puzzle\n");
+	puts("Programming is like building is like building a multilignual puzzle\n");
 	return (0);
 }
